2536aa90ea67a2c6ae8d9c06ce93ff151f698913
pazitto2208 <epazitto@gmail.com>
2024-05-02 17:10:02 +0200

    add amqp client

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import CarParameters from "./models/carParameters.js"
 import tyresPressure from "./sensors/tyresPressure.js"
 import speed from './sensors/speed.js'
 import SensorDataSender from "./sensorDataSender/sensorDataSender.js"
+import amqp from 'amqplib'
 
 async function main() {
-
+    const amqpConnection = await amqp.connect('amqps://rxlphtod:3QTOe42jj4nZdKVB3eDrYA39rR7O4oTv@crow.rmq.cloudamqp.com/rxlphtod')
+    const amqpClient = await amqpConnection.createChannel()
+    
     while (true) {
 
         const carParameters = new CarParameters(
@@ -15,7 +18,7 @@ async function main() {
         const sensorDataSender = new SensorDataSender({ carId: '660e842d951281f01eaca682', ...carParameters })
         
         try {
-            const result = await sensorDataSender.http()
+            const result = await sensorDataSender.amqp(amqpClient)
             console.log(result)
         } catch (error) {
             console.log(error)
--- client/src/sensorDataSender/sensorDataSender.js
+++ client/src/sensorDataSender/sensorDataSender.js
@@ -3,20 +3,17 @@ export default class SensorDataSender {
         this.sensorData=sensorData
     }
 
-    http () {
-        return new Promise(async (resolve, reject) => {
-            try {                
-                const response = await fetch('http://localhost:3000/parameters', {
-                    method: "POST", 
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(this.sensorData),
-                })
-                resolve({ success: true, result: await response.json(), text: 'Data inserted correctly' })  
-            } catch (error) {
-                reject({ success: false, result: error, text: 'Error inserting data' })  
+    amqp(client) {
+        return new Promise((resolve, reject) => { 
+            const dataToSend = JSON.stringify(this.sensorData)
+
+            const result = client.publish('amq.direct', '', Buffer.from(dataToSend))
+
+            if (result) {
+                resolve({ success: true, text: 'Message published correctly' })
+            } else {
+                reject({ success: true, text: 'Message published' })
             }
         })
-    }
+    }  
 }
